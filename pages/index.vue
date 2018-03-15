8dfaeaf0b46374df0bd4c1e5673ecf55c25b2495
Larry Botha <larry@fixate.it>
2018-03-15 08:10:33 +0100

    export an object with a computed property, specifying the state we want

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
   <div>Hello world</div>
 </template>
+// we export computed values here
+export default {
+  computed: {
+    // mapState returns an object
+    ...mapState({
+      // we want a counter available to the template, and that counter
+      // gets data from the state
+      counter(state) { return state.counter},
+    }),
+  }
+}
