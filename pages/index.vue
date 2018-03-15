734aa14398473cbe5df64bed56da2dc974f95049
Larry Botha <larry@fixate.it>
2018-03-15 08:50:18 +0100

    add things that I thought I already committed

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,24 @@
     <button @click="inc">+</button>
   </div>
 </template>
+
+<script>
+// get mapState, which looks like the analagous to redux's mapStateToProps
+import {mapState, mapMutations} from 'vuex';
+
+import {INCREMENT, DECREMENT} from '../store'
+
+// we export computed values here
+export default {
+  computed: {
+    // mapState returns an object
+    ...mapState({
+      // we want a counter available to the template, and that counter
+      // gets data from the state
+      counter(state) { return state.counter},
+    }),
+  },
+
   // inside a component we need a methods object in order to update state
   // This can either be an array of string names, or an object mapping names to
   // function names in the store
@@ -20,3 +38,4 @@
    )
   }
 }
+</script>
