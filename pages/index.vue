4b406a036bd82fc79ba4c7e5c3a339e719f9a97f
Larry Botha <larry@fixate.it>
2018-03-15 08:49:00 +0100

    add elements and directives for updating state

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>Hello world</div>
+  <div>
+    <!--
+    we can dispatch mutations using vue directives referencing the names we
+    define in the methods property
+    -->
+    <button @click="dec">-</button>
+    {{counter}}
+    <button @click="inc">+</button>
+  </div>
 </template>
