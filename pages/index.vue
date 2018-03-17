3f7b34618180d5a0e74bbf5adbe8e4c769e570ba
Larry Botha <larry@fixate.it>
2018-03-17 11:24:13 +0100

    get todos from vuex state

--- pages/index.vue
+++ pages/index.vue
@@ -18,31 +18,22 @@
 </template>
 
 <script>
-// get mapState, which looks like the analagous to redux's mapStateToProps
 import {mapState, mapMutations} from 'vuex';
 
-import {INCREMENT, DECREMENT} from '../store'
-
-// we export computed values here
 export default {
   computed: {
-    // mapState returns an object
     ...mapState({
-      // we want a counter available to the template, and that counter
-      // gets data from the state
-      counter(state) { return state.counter},
+      // Get the todos from our store
+      // We could call them anything here, as long as inside the function we are
+      // referencing the actual state property
+      todos(state) {
+        return state.todos;
+      },
     }),
   },
 
-  // inside a component we need a methods object in order to update state
-  // This can either be an array of string names, or an object mapping names to
-  // function names in the store
   methods: {
-    ...mapMutations({
-      'inc': INCREMENT,
-      'dec': DECREMENT
-    }
-   )
-  }
-}
+    ...mapMutations({}),
+  },
+};
 </script>
